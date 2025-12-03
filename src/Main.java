2e84f80550e87f92ab84f57f805a73bb61ac7122
Test <test@test.com>
2025-12-03 15:16:22 +0300

    Test by the other

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
 
     static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Test1");
-        System.out.println("Test2");
+        System.out.println("Hello World!");
     }
 }
